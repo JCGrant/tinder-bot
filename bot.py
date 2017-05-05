3b31225e716adb0b55742f1b3de9ba01b5f80293
JCGrant <jamescolin.grant@gmail.com>
2017-05-05 22:37:08 +0100

    Renew auth token programmatically

--- bot.py
+++ bot.py
@@ -1,8 +1,17 @@
 import os
 import pynder
-import config
+from token_util import get_token
 
 
 class Bot:
-    def __init__(self, fb_id=None, fb_token=None):
-        self.session = pynder.Session(facebook_id=fb_id, facebook_token=fb_token)
+    def __init__(self, fb_id, fb_email, fb_password):
+        self.fb_id = fb_id
+        self.fb_email = fb_email
+        self.fb_password = fb_password
+        self.renew_session()
+    
+    def renew_session(self):
+        print("Obtaining new token")
+        new_token = get_token(self.fb_email, self.fb_password)
+        print("Creating new pynder Session")
+        self.session = pynder.Session(facebook_id=self.fb_id, facebook_token=new_token)
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import config
 from bot import Bot
 
-fb_id = config.FACEBOOK_ID
-fb_token = config.FACEBOOK_TOKEN
-bot = Bot(fb_id, fb_token)
+bot = Bot(config.FACEBOOK_ID, config.FACEBOOK_EMAIL, config.FACEBOOK_PASSWORD)
