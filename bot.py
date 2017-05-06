64e6cb2670c10ecf0d037597a17d9395aadfabac
JCGrant <jamescolin.grant@gmail.com>
2017-05-06 16:15:48 +0100

    Get Facebook ID programatically using fb_token

--- bot.py
+++ bot.py
@@ -1,16 +1,15 @@
 import pynder
-from token_util import get_token
+from fb_auth_util import get_auth_details
 
 
 class Bot:
-    def __init__(self, fb_id, fb_email, fb_password):
-        self.fb_id = fb_id
+    def __init__(self, fb_email, fb_password):
         self.fb_email = fb_email
         self.fb_password = fb_password
         self.renew_session()
     
     def renew_session(self):
-        print("Obtaining new token")
-        new_token = get_token(self.fb_email, self.fb_password)
+        print("Obtaining Facebook ID and Token")
+        fb_id, fb_token = get_auth_details(self.fb_email, self.fb_password)
         print("Creating new pynder Session")
-        self.session = pynder.Session(facebook_id=self.fb_id, facebook_token=new_token)
+        self.session = pynder.Session(facebook_id=fb_id, facebook_token=fb_token)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import config
 from bot import Bot
 
-bot = Bot(config.FACEBOOK_ID, config.FACEBOOK_EMAIL, config.FACEBOOK_PASSWORD)
+bot = Bot(config.FACEBOOK_EMAIL, config.FACEBOOK_PASSWORD)
