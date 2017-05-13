f4da4a8770c36b82a257c18b22b055d11b21a52c
JCGrant <jamescolin.grant@gmail.com>
2017-05-13 06:57:48 +0100

    Add Match route

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,32 @@ import {
 } from 'react-router-dom'
 import './App.css';
 
+
+class Match extends Component {
+  constructor({match}) {
+    super();
+    this.state = {
+    }
+    fetch('/matches/' + match.params.matchId + '/')
+      .then((response) => response.json())
+      .then((tinderMatch) => this.setState({tinderMatch}));
+  }
+
+  render() {
+    const match = this.state.tinderMatch;
+    if (!match) {
+      return null;
+    }
+    return (
+      <div className="match">
+        <Link to="/">Back</Link>
+        <h1>{match.person.name}</h1>
+        <img src={match.person.photos[0].processedFiles[3].url} alt=""/>
+      </div>
+    )
+  }
+}
+
 const MatchItem = ({match}) => (
   <div className="match">
     <Link to={"/matches/" + match._id}>{match.person.name}</Link><br/>
@@ -41,6 +67,7 @@ const App = () => (
   <Router>
     <div>
       <Route exact path="/" component={Home} />
+      <Route path="/matches/:matchId" component={Match} />
     </div>
   </Router>
 );
