5493347200d87bf9a98e208a4b40f200ecbcd3e2
JCGrant <jamescolin.grant@gmail.com>
2017-05-14 15:37:38 +0100

    Remove update matches threading code from bot

--- api.py
+++ api.py
@@ -8,20 +8,17 @@ bot = Bot(config.FACEBOOK_EMAIL, config.FACEBOOK_PASSWORD)
 app = Flask(__name__)
 api = Api(app)
 
-def get_matches():
-    if bot.running:
-        return bot.matches_json
-    else:
-        return bot.session._api.matches(None)
+def get_updates():
+    return bot.session._api.updates(None)
 
 class Matches(Resource):
     def get(self):
-        return get_matches()
+        return get_updates().get('matches')
 
 class Match(Resource):
     def get(self, match_id):
         match = {}
-        for m in get_matches():
+        for m in get_updates().get('matches'):
             if m['_id'] == match_id:
                 match = m
                 break
@@ -31,5 +28,4 @@ api.add_resource(Matches, '/matches/')
 api.add_resource(Match, '/matches/<match_id>/')
 
 if __name__ == '__main__':
-    bot.start()
     app.run(debug=True)
--- bot.py
+++ bot.py
@@ -1,30 +1,14 @@
 import pynder
 from fb_auth_util import get_auth_details
-from threading import Thread
-
 
 class Bot:
     def __init__(self, fb_email, fb_password):
         self.fb_email = fb_email
         self.fb_password = fb_password
         self.renew_session()
-        self.running = False
-        self.matches_json = []
     
     def renew_session(self):
         print("Obtaining Facebook ID and Token")
         fb_id, fb_token = get_auth_details(self.fb_email, self.fb_password)
         print("Creating new pynder Session")
         self.session = pynder.Session(facebook_id=fb_id, facebook_token=fb_token)
-
-    def keep_fetching_matches(self):
-        while self.running:
-            self.matches_json = self.session._api.matches(None)
-
-    def start(self):
-        bot_thread = Thread(target=self.keep_fetching_matches, daemon=True)
-        self.running = True
-        bot_thread.start()
-
-    def stop(self):
-        self.running = False
