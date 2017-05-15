df1ec2ae759fcd004c27a9b3ac23656fa124af04
JCGrant <jamescolin.grant@gmail.com>
2017-05-15 02:27:55 +0100

    Keep fetching updates in separate thread

--- api.py
+++ api.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask_restful import Resource, Api
 from flask_socketio import SocketIO
 from werkzeug.contrib.cache import SimpleCache
+from threading import Thread
+import time
 
 bot = Bot(config.FACEBOOK_EMAIL, config.FACEBOOK_PASSWORD)
 
@@ -12,6 +14,14 @@ api = Api(app)
 socket = SocketIO(app)
 
 cache = SimpleCache()
+
+def keep_fetching_updates():
+    while True:
+        cache.set('updates', bot.session._api.updates(None), timeout=5 * 60)
+        time.sleep(5)
+
+def start_fetching_updates():
+    Thread(target=keep_fetching_updates, daemon=True).start()
 
 def get_updates():
     if not cache.has('updates'):
@@ -38,4 +48,5 @@ api.add_resource(Matches, '/matches/')
 api.add_resource(Match, '/matches/<match_id>/')
 
 if __name__ == '__main__':
+    start_fetching_updates()
     socket.run(app, debug=True)
